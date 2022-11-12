a6451b8436c024907533e28702626c61ed929661
Mantou1233 <rwkn100@gmail.com>
2022-11-12 19:43:43 +0800

    v3-init

--- src/plugins/utility/src/index.ts
+++ src/plugins/utility/src/index.ts
@@ -3,8 +3,8 @@ import { Client, Embed, EmbedBuilder, User } from "discord.js";
 import axios from "axios";
 import translate from "@iamtraction/google-translate";
 
-import Manager from "~/core/Manager";
-import { UserProfile, GuildProfile } from "~/core/Profile";
+import Manager from "@core/Manager";
+import { UserProfile, GuildProfile } from "@core/Profile";
 
 import { convertSnowflakeToDate } from "@services/snowflake";
 import { getUser } from "@services/gets";
