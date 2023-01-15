800058dbdb7b9f26f538c6be0d1aec70f6984b8a
Mantou1233 <lowkey@ducker.ml>
2023-01-15 11:44:33 +0800

    rm dist

--- dist/services/database.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const quickmongo_1 = require("quickmongo");
-quickmongo_1.Database;
-//# sourceMappingURL=database.js.map
